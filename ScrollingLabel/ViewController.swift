fe3a405a642fd049b4b13f057bf306b386ab6158
Pablo Balduz <pbalduz@gmail.com>
2019-05-27 20:42:03 +0200

    Initial Commit

---
+++ ScrollingLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollingLabel
+//
+//  Created by Pablo Balduz on 27/05/2019.
+//  Copyright © 2019 Pablo Balduz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
